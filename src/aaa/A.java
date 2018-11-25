edcadd1b2b1031365f9c9358b1762dbc500adbd8
amin <amin@qq.com>
2018-11-25 16:51:17 +0800

    ‘first’

--- src/aaa/A.java
+++ src/aaa/A.java
@@ -5,11 +5,6 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1111");
-		System.out.println("1111");
-		System.out.println("1111");
-		System.out.println("1111");
-		System.out.println("1111");
-		
 	}
 
 }
